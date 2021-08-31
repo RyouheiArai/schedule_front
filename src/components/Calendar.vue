3e79d53e9da8ff2c7bcb8ed68dae727bee871215
ryoheiarai <arai0684@gmail.com>
2021-09-01 01:09:09 +0900

    コミット漏れ[765f3098367dc4cbfb87cf8dc646e7cc69c13864]

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,68 +3,85 @@
 </template>
 
 <script>
-import FullCalendar from '@fullcalendar/vue'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import timeGridPlugin from "@fullcalendar/timegrid"
-import jaLocale from "@fullcalendar/core/locales/ja"
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
 
 export default {
   components: {
-    FullCalendar // make the <FullCalendar> tag available
+    FullCalendar, // make the <FullCalendar> tag available
   },
   data() {
+    // カレンダーデータ取得
+    var events = this.doFetchAllschedules;
+
     return {
       calendarOptions: {
-				dateClick: this.handleDateClick, //日付クリック
-				eventClick: this.handleEventClick, //スケジュールクリック
-				// カレンダーヘッダーのデザイン
-				headerToolbar: {
-					left: 'prev,next today',
-					center: 'title',
-					right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-				},
-				navLinks: true,
+        dateClick: this.handleDateClick, //日付クリック
+        eventClick: this.handleEventClick, //スケジュールクリック
+        // カレンダーヘッダーのデザイン
+        headerToolbar: {
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+        },
+        navLinks: true,
         businessHours: true,
         editable: true,
-        plugins: [ dayGridPlugin, interactionPlugin, timeGridPlugin ],
-        initialView: 'dayGridMonth',
-				locale: jaLocale, // 日本語化
-				events: [
-                    {
-                        id: '1',
-                        title: 'A社　会議',
-                        start: '2021-08-31 19:00',
-												end:'2021-08-31 19:30',
-                        url: '#'
-                    }
-                ],
-				
-      }
-    }
+        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+        initialView: "dayGridMonth",
+        locale: jaLocale, // 日本語化
+        events: events,
+      },
+    };
   },
-	methods: {
-    handleDateClick: function(arg) {
-			this.$router.push({
-      name: 'Register',
-      params: {
-        date: arg.dateStr,
-      },
-    });
-		console.log("カレンダー画面"+arg.dateStr)
+  methods: {
+    handleDateClick: function (arg) {
+      this.$router.push({
+        name: "Register",
+        params: {
+          date: arg.dateStr,
+        },
+      });
+      console.log("カレンダー画面" + arg.dateStr);
     },
-		handleEventClick: function(info) {
-		
-			this.$router.push({
-      name: 'Edit',
-      params: {
-				id:info.event.id,
-				title: info.event.title,
-        start: info.event.start,
-				end: info.event.end,
-      },
-    });
-    }
-	}
+    handleEventClick: function (info) {
+      this.$router.push({
+        name: "Edit",
+        params: {
+          id: info.event.id,
+          title: info.event.title,
+          start: info.event.start,
+          end: info.event.end,
+        },
+      });
+    },
+    // スケジュールを取得する
+    doFetchAllschedules() {
+      var events = [];
+
+      this.axios
+        .get(process.env.VUE_APP_API_ENDPOINT + "/fetchAllschedules")
+        .then((response) => {
+          if (response.status != 200) {
+            throw new Error("レスポンスエラー");
+          } else {
+            var resultschedules = response.data;
+            for (let i = 0; i < resultschedules.length; i++) {
+              var e = resultschedules[i]; // some calendar event
+              events.push({
+                title: e.company,
+                start: e.start_date,
+                end: e.end_date,
+              });
+
+              return events;
+            }
+          }
+        });
+    },
+  },
 };
 </script>
