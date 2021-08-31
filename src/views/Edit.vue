765f3098367dc4cbfb87cf8dc646e7cc69c13864
ryoheiarai <arai0684@gmail.com>
2021-09-01 01:07:31 +0900

    設定ファイル追加、カレンダーデータ取得

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -6,7 +6,7 @@
         type="text"
         name="scheduleName"
         v-model="scheduleName"
-        v-bind:class="{'alert-color': !validate }"
+        v-bind:class="{ 'alert-color': !validate }"
         value
         size="40"
         placeholder="予定を入力してください※必須"
@@ -14,20 +14,40 @@
     </p>
     <p>
       日付：
-      <input type="date" name="scheduleDate" v-model="scheduleDay" value size="40" />
+      <input
+        type="date"
+        name="scheduleDate"
+        v-model="scheduleDay"
+        value
+        size="40"
+      />
     </p>
     <p>
       開始時間：
-      <input type="time" name="scheduleStartTime" v-model="scheduleStartTime" value size="40" />
+      <input
+        type="time"
+        name="scheduleStartTime"
+        v-model="scheduleStartTime"
+        value
+        size="40"
+      />
     </p>
     <p>
       終了時間：
-      <input type="time" name="scheduleEndTime" v-model="scheduleEndTime" value size="40" />
+      <input
+        type="time"
+        name="scheduleEndTime"
+        v-model="scheduleEndTime"
+        value
+        size="40"
+      />
     </p>
 
     <!-- 変更ボタン -->
-    <button v-on:click="doChangeschedule" v-bind:disabled="!isEntered">変更</button>
-		<!-- 削除ボタン -->
+    <button v-on:click="doChangeschedule" v-bind:disabled="!isEntered">
+      変更
+    </button>
+    <!-- 削除ボタン -->
     <button v-on:click="doDeleteschedule">削除</button>
 
     <hr />
@@ -35,8 +55,6 @@
 </template>
 
 <script>
-
-
 export default {
   // 「el」プロパティーで、Vueの表示を反映する場所＝HTML要素のセレクター（id）を定義
   //el: "#app",
@@ -70,31 +88,31 @@ export default {
       return isEnteredscheduleName;
     },
   },
-	created() {
-		// 日付をカレンダーのクリックした日付から受け取る
-		if(this.$route.params.id != null){
-			var startDate = this.$route.params.start;
-			var endDate = this.$route.params.end;
+  created() {
+    // 日付をカレンダーのクリックした日付から受け取る
+    if (this.$route.params.id != null) {
+      var startDate = this.$route.params.start;
+      var endDate = this.$route.params.end;
 
-			// 開始日付取得
-			var startYear = startDate.getFullYear();
-			var startMonth = ("0"+(startDate.getMonth() + 1)).slice(-2);
-			var startDay =  ("0"+startDate.getDate()).slice(-2);
-			// 開始時間取得
-			var startHour =  ("0"+startDate.getHours()).slice(-2);
-			var startMinute = ("0"+startDate.getMinutes()).slice(-2);
-			// 終了時間取得
-			var endHour = ("0"+endDate.getHours()).slice(-2);
-			var endMinute = ("0"+endDate.getMinutes()).slice(-2);
+      // 開始日付取得
+      var startYear = startDate.getFullYear();
+      var startMonth = ("0" + (startDate.getMonth() + 1)).slice(-2);
+      var startDay = ("0" + startDate.getDate()).slice(-2);
+      // 開始時間取得
+      var startHour = ("0" + startDate.getHours()).slice(-2);
+      var startMinute = ("0" + startDate.getMinutes()).slice(-2);
+      // 終了時間取得
+      var endHour = ("0" + endDate.getHours()).slice(-2);
+      var endMinute = ("0" + endDate.getMinutes()).slice(-2);
 
-			this.schedulesId = this.$route.params.id;
-			this.scheduleName = this.$route.params.title;
-			this.scheduleDay = startYear + '-' + startMonth + '-' + startDay;
-			this.scheduleStartTime = startHour + ':' + startMinute;
-			this.scheduleEndTime =  endHour + ':' + endMinute;
+      this.schedulesId = this.$route.params.id;
+      this.scheduleName = this.$route.params.title;
+      this.scheduleDay = startYear + "-" + startMonth + "-" + startDay;
+      this.scheduleStartTime = startHour + ":" + startMinute;
+      this.scheduleEndTime = endHour + ":" + endMinute;
 
-			console.log(this.scheduleStartTime);
-		} 
+      console.log(this.scheduleStartTime);
+    }
   },
   methods: {
     handleDateClick: function (arg) {
@@ -107,7 +125,7 @@ export default {
 
       // サーバへ送信するパラメータ
       const params = new URLSearchParams();
-			params.append("scheduleId", this.schedulesId);
+      params.append("scheduleId", this.schedulesId);
       params.append("scheduleName", this.scheduleName);
       params.append("scheduleMemo", this.scheduleMemo);
       params.append(
@@ -119,24 +137,27 @@ export default {
         this.scheduleDay + space + this.scheduleEndTime + scheduleSeconds
       );
 
-      this.axios.post("/changeschedule", params).then((response) => {
-        if (response.status != 200) {
-          throw new Error("レスポンスエラー");
-        } 
-      });
+      this.axios
+        .post(process.env.VUE_APP_API_ENDPOINT + "/changeschedule", params)
+        .then((response) => {
+          if (response.status != 200) {
+            throw new Error("レスポンスエラー");
+          }
+        });
     },
-		// スケジュールを削除する
+    // スケジュールを削除する
     doDeleteschedule() {
-
       // サーバへ送信するパラメータ
       const params = new URLSearchParams();
-			params.append("scheduleId", this.schedulesId);
+      params.append("scheduleId", this.schedulesId);
 
-      this.axios.post("/deleteschedule", params).then((response) => {
-        if (response.status != 200) {
-          throw new Error("レスポンスエラー");
-        } 
-      });
+      this.axios
+        .post(process.env.VUE_APP_API_ENDPOINT + "/deleteschedule", params)
+        .then((response) => {
+          if (response.status != 200) {
+            throw new Error("レスポンスエラー");
+          }
+        });
     },
   },
 };
